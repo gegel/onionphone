0dbc85e64d3f4cd719bd906b432d243904cdbe6a
Oleksandr Natalenko <oleksandr@natalenko.name>
2014-10-23 11:53:47 +0300

    lpc10: zero arrays correctly in initializers

--- libcodecs/lpc10/initial.c
+++ libcodecs/lpc10/initial.c
@@ -5,6 +5,7 @@ routine is entered
 
 #include "lpcdefs.h"
 #include <string.h>
+#include <ophtools.h>
 
 /* analys */
 extern float lparray[LBUFH-LBUFL+1], ivarray[PWINH-PWINL+1];
@@ -33,40 +34,30 @@ extern float rci[MAXORD][11], rmsi[11], pc[MAXORD];
 
 void initialize1(void)
 {
-int i, j;
 /* ---------------------- bsynz -------------------------- */
-for(i=0;i<MAXPIT+MAXORD;i++)	{
-  exc[i] = 0.0;
-  exc2[i] = 0.0;
-}
+memzero(exc, (MAXPIT + MAXORD) * sizeof(float));
+memzero(exc2, (MAXPIT + MAXORD) * sizeof(float));
 
 /* ---------------------- dyptrk -------------------------- */
-memset(p, 0, 60*2);
-for(i=0;i<60;i++)
-  s[i] = 0.0;
+memzero(p, 60 * 2 * sizeof(int));
+memzero(s, 60 * sizeof(float));
 	
 /* ----------------------- decode ----------------------- */
-memset(dpit, 0, 3);
-memset(drms, 0, 3);
-memset(drc, 0, MAXORD*3);
+memzero(dpit, 3 * sizeof(int));
+memzero(drms, 3 * sizeof(int));
+memzero(drc, MAXORD * 3 * sizeof(int));
 
 /* ----------------------- synths ----------------------- */
-memset(ipiti, 0, 11);
-memset(ivuv, 0, 11);
-for(i=0;i<10;i++)	{
-	rmsi[i] = 0.0;
-	pc[i] = 0.0;
-}
-for(i=0;i<MAXORD;i++)
-  for(j=0;j<11;j++)
-    rci[i][j] = 0.0;
-
+memzero(ipiti, 11 * sizeof(int));
+memzero(ivuv, 11 * sizeof(int));
+memzero(rmsi, 11 * sizeof(float));
+memzero(pc, MAXORD * sizeof(float));
+memzero(rci, MAXORD * 11 * sizeof(float));
 }
 
 
 void initialize2(void)
 {
-	int i;
 /* ----------------------- analys.c ----------------------- */
 /* allocate space for the arrays */
 inbuf = inarray;
@@ -74,14 +65,10 @@ lpbuf = lparray;
 ivbuf = ivarray;
 pebuf = pearray;
 /* Initialize space created */
-for(i=0;i<SBUFH-SBUFL+1;i++)	{
-	inarray[i] = 0.0;
-	pearray[i] = 0.0;
-}
-for(i=0;i<LBUFH-LBUFL+1;i++)
-  lparray[i] = 0.0;
-for(i=0;i<PWINH-PWINL+1;i++)
-  ivarray[i] = 0.0;
+memzero(inarray, (SBUFH - SBUFL + 1) * sizeof(float));
+memzero(pearray, (SBUFH - SBUFL + 1) * sizeof(float));
+memzero(lparray, (LBUFH - LBUFL + 1) * sizeof(float));
+memzero(ivarray, (PWINH - PWINL + 1) * sizeof(float));
 
 /* align C arrays to FORTRAN indexing */
 inbuf -= SBUFL;
@@ -99,19 +86,12 @@ awin[1][AF-1] = DVWINH;
 awin[0][1] = 0;
 awin[1][1] = 0;
 
-for(i=0;i<MAXORD;i++)
-  rcbuf[0][i] = rcbuf[1][i] = rcbuf[2][i] = 0.0;
+memzero(rcbuf, MAXORD * AF * sizeof(float));
+memzero(rmsbuf, AF * sizeof(float));
+memzero(amdf, LTAU * sizeof(float));
 
-for(i=0;i<AF;i++)
-  rmsbuf[i] = 0.0;
-
-for(i=0;i<LTAU;i++)
-  amdf[i] = 0.0;
-
-memset(voibuf, 0, (AF+1)*2);
+memzero(voibuf, (AF + 1) * 2 * sizeof(int));
 
 /* onset */
-for(i=0;i<16;i++)
-  l2buf[i] = 0.0;
-
+memzero(l2buf, 16 * sizeof(float));
 }
