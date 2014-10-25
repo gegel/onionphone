ea4f9a5fba4d3eb053f0e8a801490a8dd34c3716
Oleksandr Natalenko <oleksandr@natalenko.name>
2014-10-25 21:34:50 +0300

    libdesktop: remove extra typedefs from ophmconsts.h

--- common/inc/ophmconsts.h
+++ common/inc/ophmconsts.h
@@ -27,18 +27,5 @@
 #define exp2(A) (exp(A / M_LOG2E))
 #endif
 
-#define F FLOAT
-typedef float FLOAT;
-typedef float Float32;
-typedef double Float64;
-typedef short INT16;
-typedef int INT32;
-typedef char Word8;
-typedef short Word16;
-typedef int Word32;
-typedef unsigned char UWord8;
-typedef unsigned int UWord32;
-typedef int Flag;
-
 #endif /* _OPHMCONSTS_H_ */
 
