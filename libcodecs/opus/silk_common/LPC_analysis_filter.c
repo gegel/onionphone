3509f94721c4e2ac87405d77943fc14b92b944cb
Oleksandr Natalenko <oleksandr@natalenko.name>
2014-10-29 17:45:26 +0200

    opus: include ophtools.h to LPC_analysis_filter.c

--- libcodecs/opus/silk_common/LPC_analysis_filter.c
+++ libcodecs/opus/silk_common/LPC_analysis_filter.c
@@ -27,6 +27,8 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 ***********************************************************************/
 
+#include <ophtools.h>
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
